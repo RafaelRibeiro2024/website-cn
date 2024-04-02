8a2df6a7fc873dcb7efefb5ac79c5462186308ed
Rafael Ribeiro <Rafael.ribeiro@ipcbcampus.pt>
2024-04-03 00:23:05 +0100

    First commit

--- index.js
+++ index.js
@@ -1,64 +1,216 @@
 const express = require('express');
-const multer = require('multer');
-const fs = require('fs'); // Import the fs module
+const bodyParser = require('body-parser');
 const app = express();
-
-// Multer configuration
-const upload = multer({ dest: 'uploads/' }); // Define the destination folder for uploaded files
-
-// Importing the BlobServiceClient library from Azure Storage
+const fileupload = require('express-fileupload');
+const { CosmosClient } = require("@azure/cosmos");
 const { BlobServiceClient } = require("@azure/storage-blob");
+app.use(fileupload({
+    useTempFiles: true,
+    tempFileDir: "/tmp",
+}));
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/dfsd', (req, res) => {
-    const htmlString = `
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>Upload de Arquivos para o Azure Blob Storage</title>
-    </head>
-    <body>
-        <form action="/upload" method="post" enctype="multipart/form-data">
-                <input type="file" name="nome_do_arquivo" accept="image/*" multiple />
-            <button type="submit">Enviar Arquivo</button>
-        </form>
-    </body>
-    </html>
+const connString = 'DefaultEndpointsProtocol=https;AccountName=trabalhoteste;AccountKey=N9MYIECYga9ETsbXQWSMAKnNo+qy/uzprQUyG50YDoI84nwOM4r8pzs8ByergbOl46sQvEQhuE6w+AStVwrvlQ==;EndpointSuffix=core.windows.net';
+const blobServiceClient = BlobServiceClient.fromConnectionString(connString);
+
+const itemsAprovatos = [];
+const items = [];
+const endpoint = "https://cosmos-ricardo0723059.documents.azure.com:443/";
+const key = "IvENfgz69qDwnzTnPAXutetLc7jKbpliyHXpAPP30dXK23PIj9fJZF2LOjvb2Y04wpJ3MxmjsabPACDb142tfQ==";
+
+const client = new CosmosClient({ endpoint, key });
+
+let proximoId;
+let db;
+let cont;
+
+
+
+async function ProxID() {
+    try {
+        const { database } = await client.databases.createIfNotExists({ id: "Eventos" });
+        db = database;
+        const { container } = await database.containers.createIfNotExists({ id: "Evento" });
+        cont = container;
+
+        const querySpec = {
+            query: 'SELECT TOP 1 c.id FROM c ORDER BY c.id DESC'
+        };
+
+        const { resources: results } = await container.items.query(querySpec).fetchAll();
+
+        if (results.length > 0) {
+            const ultimoID = results[0].id;
+            const prox = parseInt(ultimoID) + 1;
+            proximoId = prox;
+        } else {
+            proximoId = 1;
+        }
+    } catch (error) {
+        console.error("Error in ProxID function:", error);
+        throw error;
+    }
+}
+async function todosAprovatos() {
+    const { database } = await client.databases.createIfNotExists({ id: "Eventos" });
+    db = database;
+    const { container } = await database.containers.createIfNotExists({ id: "Evento" });
+    cont = container;
+    const { resources } = await cont.items
+    .query("SELECT * from c where c.aprovado = true")
+    .fetchAll();
+    for (const evento of resources) {
+        itemsAprovatos.push({Nome: evento.nome, Descricao: evento.descrição, Data: evento.start_date, Local: evento.local, Ocupacao: evento.ocupacao, Imagem: evento.link_imagens});
+    }
+}
+
+async function Todos() {
+    const { database } = await client.databases.createIfNotExists({ id: "Eventos" });
+    db = database;
+    const { container } = await database.containers.createIfNotExists({ id: "Evento" });
+    cont = container;
+
+    const { resources } = await container.items
+    .query("SELECT * from c")
+    .fetchAll();
+    for (const evento of resources) {
+        items.push({Nome: evento.nome, Descricao: evento.descrição, Data: evento.start_date, Local: evento.local, Ocupacao: evento.ocupacao, Imagem: evento.link_imagens});
+    }
+}
+
+Todos();
+todosAprovatos();
+
+
+
+app.get('/', (req, res) => {
+    const html = `
+        <h1>Lista de eventos</h1>
+        <ul>
+            ${items.map((item, index) => `
+            <li>
+                <strong>Nome:</strong> ${item.Nome}<br>
+                <strong>Descrição:</strong> ${item.Descricao}<br>
+                <strong>Data:</strong> ${item.Data}<br>
+                <strong>Local:</strong> ${item.Local}<br>
+                <strong>Ocupação:</strong> ${item.Ocupacao}<br>
+                <strong>Imagem:</strong> <img src="${item.Imagem}" alt="" width="200" height="200"><br>
+                <button onclick="location.href='/Detalhes/${index}'">Mais detalhes</button>
+
+        </li>`).join('')}
+        </ul>
+        <button onclick="location.href='/criarevento'">Criar um evento</button>
+        <button onclick="location.href='/SoAprovados'">So aprovados</button>
+        <br>
+        <br>
+        
     `;
-    res.send(htmlString);
+    res.send(html);
 });
 
-// Your file upload handling function
-const uploadFiles = async (files) => {
+app.get('/SoAprovados', (req, res) => {
+
+    const html = `
+        <h1>Lista de eventos aprovados</h1>
+        <ul>
+            ${itemsAprovatos.map((itemA, index) => `
+            <li>
+                <strong>Nome:</strong> ${itemA.Nome}<br>
+                <strong>Descrição:</strong> ${itemA.Descricao}<br>
+                <strong>Data:</strong> ${itemA.Data}<br>
+                <strong>Local:</strong> ${itemA.Local}<br>
+                <strong>Ocupação:</strong> ${itemA.Ocupacao}<br>
+                <strong>Imagem:</strong> <img src="${itemA.Imagem}" alt="" width="200" height="200"><br>
+                <button onclick="location.href='/Detalhes/${index}'">Mais detalhes</button>
+        </li>`).join('')}
+        </ul>
+        <button onclick="location.href='/'">Voltar para a página principal</button>
+        <br>
+        <br>
+        
+    `;
+    res.send(html);
+});
+app.get('/Detalhes/:index', (req, res) => {
+    const index = parseInt(req.params.index);
+    const detalhes = `
+            <h1>Detalhes do evento</h1>
+            <h2>Nome do evento:</h2> ${items[index].Nome}
+            <h2>Descrição:</h2> ${items[index].Descricao}
+            <h2>Data:</h2> ${items[index].Data}
+            <h2>Local: </h2>${items[index].Local}
+            <h2>Ocupação: </h2>${items[index].Ocupacao}
+            <h2>Imagem:</h2><img src="${items[index].Imagem}" alt="Imagem do evento ${index}" width="200" height="200">
+            <br>
+            <br>
+            <button onclick="location.href='/'">Voltar para a página principal</button>
+
+        `;
+        res.send(detalhes);
+    });
+
+app.get('/criarevento', (req, res) => {
+    const html = `
+        <h1>Criar Evento</h1>
+        <form action="/criareventos" method="post" enctype="multipart/form-data">
+            <input type="text" name="Nome" placeholder="Nome do evento" required><br>
+            <input type="text" name="Descricao" placeholder="Descricao" required><br>
+            <input type="text" name="Organizadores" placeholder="Organizadores" required><br>
+            <input type="date" name="Data"  placeholder="Data Inicio" required><br>
+            <input type="date" name="DataFim"  placeholder="Data Fim" required><br>
+            <input type="text" name="Local" placeholder="Local" required><br>
+            <input type="number" name="Ocupacao" placeholder="Ocupacao" required><br>
+            <input type="file"  name="image"  accept="image/*" required><br>
+            <button type="submit">Criar</button>
+        </form>
+        <button onclick="location.href='/criar'">Voltar para a página principal</button>
+    `;
+    res.send(html);
+});
+app.post('/criareventos', async (req, res) => {
     try {
-        const blobSasUrl = "https://testetrabalho.blob.core.windows.net/?sv=2022-11-02&ss=b&srt=co&sp=wctf&se=2024-03-26T22:07:37Z&st=2024-03-26T14:07:37Z&spr=https&sig=3sorRUG3Za1c03ufj%2BQBuJXZJXcs%2FJ7LEwNKGa3N96Q%3D";
-        const blobServiceClient = new BlobServiceClient(blobSasUrl);
-        const containerName = "teste-containers";
+        //await ProxID(); 
+        console.log(proximoId);
+        
+        const {image} = req.files;
+        const imgPath = image.tempFilePath;
+        let imgUrl='';
+        const { Nome, Descricao, Organizadores, Data, DataFim, Local, Ocupacao } = req.body;
+        
+        let containerName = "teste-containers"; 
         const containerClient = blobServiceClient.getContainerClient(containerName);
 
-        const promises = [];
-        for (const file of files) {
-            // Read the file locally
-            const fileContent = fs.readFileSync(file.path);
-            // Upload to Azure Blob Storage
-            const blockBlobClient = containerClient.getBlockBlobClient(file.originalname); // Changed file.name to file.originalname
-            promises.push(blockBlobClient.uploadData(fileContent, file.size));
-        }
-        await Promise.all(promises);
-        console.log('Done.');
-    }
-    catch (error) {
-        console.error(error.message);
-    }
-};
+        const blobName = `${image.name}`; 
+        const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+        await blockBlobClient.uploadFile(imgPath);
 
-// Route for file upload
-app.post('/upload', upload.array('nome_do_arquivo'), (req, res) => { // Use upload.array() to handle multiple files
-    // Call the file upload function
-    uploadFiles(req.files)
-        .then(() => res.send('Arquivos enviados com sucesso para o Azure Blob Storage.'))
-        .catch(() => res.status(500).send('Ocorreu um erro durante o upload dos arquivos.'));
+        imgUrl = blockBlobClient.url;
+
+        var item = {
+            'nome': Nome,
+            'organizadores': Organizadores,
+            'descrição': Descricao, 
+            'local': Local,
+            'start_date': Data,
+            'end_date': DataFim,
+            'aprovado': false,
+            'ocupacao': Ocupacao,
+            'link_imagens': imgUrl,
+        };
+        await
+        cont.items.create(item);
+
+        const html = `
+            <h1>Evento criado com sucesso</h1>
+            <button onclick="location.href='/'">Voltar para a página principal</button>`;
+        res.send(html);
+    } catch (error) {
+        console.error(error.message);
+        return res.status(500).send('Error creating event');
+    }
 });
 
 app.listen(3000, () => {
-    console.log('Servidor rodando na porta 3000');
+    console.log('Server no port 3000');
 });
